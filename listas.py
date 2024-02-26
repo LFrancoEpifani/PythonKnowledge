3bd7262b6816cb0ce8544256acb4b95543ca9fd8
LucianoEsplai <lfrancoepifani@gmail.com>
2024-02-26 16:37:46 +0100

    First Exercises

---
+++ listas.py
@@ -0,0 +1,23 @@
+listaAlimentos = ['Manzana', True, 5, 78.35];
+
+listaBebidas = ['Agua', 'Cafe', 'Soda'];
+
+print(listaAlimentos[:3])
+
+listaBebidas.append('CocaCola')
+
+print(listaBebidas)
+
+listaBebidas.insert(2,"Leche")
+
+print(listaBebidas)
+
+listaAlimentos.extend(['Sandia', 'Melon', 'Kiwi'])
+
+print(listaAlimentos)
+
+print(listaAlimentos.index("Manzana"))
+
+print(True in listaAlimentos)
+
+
---
+++ pruebas.py
@@ -0,0 +1,7 @@
+contador = 3;
+
+while contador > 0:
+	print(f"Contador: {contador}")
+	contador -= 1
+else: 
+	print("!Despegue!")
